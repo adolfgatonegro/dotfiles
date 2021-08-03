5c5644a92e620248faf8891c4c59dfafe3dd8db6
AdolfGatonegro <github@gatoneg.ro>
2021-08-03 11:32:13 -0500

    Switched Super/alt + tab behaviour.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -166,10 +166,10 @@ for i in groups:
 
 #CHANGE WORKSPACES
         Key([mod], i.name, lazy.group[i.name].toscreen()),
-#        Key([mod], "Tab", lazy.screen.next_group()),
-#        Key([mod, "shift" ], "Tab", lazy.screen.prev_group()),
-        Key(["mod1"], "Tab", lazy.screen.next_group()),
-        Key(["mod1", "shift"], "Tab", lazy.screen.prev_group()),
+        Key([mod], "Tab", lazy.screen.next_group()),
+        Key([mod, "shift" ], "Tab", lazy.screen.prev_group()),
+#        Key(["mod1"], "Tab", lazy.screen.next_group()),
+#        Key(["mod1", "shift"], "Tab", lazy.screen.prev_group()),
 
 # MOVE WINDOW TO SELECTED WORKSPACE 1-10 AND STAY ON WORKSPACE
         #Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
