5899c34d13b69881a3544051c50de62b3d86f505
AdolfGatonegro <github@gatoneg.ro>
2021-08-19 17:45:07 -0500

    Added explicit refresh rate setting for both displays to avoid main one defaulting to 60Hz on toggle.

--- .config/qtile/scripts/display-toggle.sh
+++ .config/qtile/scripts/display-toggle.sh
@@ -17,10 +17,10 @@
 if xrandr --listactivemonitors | grep "DP-1"
 then
 	echo "Switching off the second display." && \
-	xrandr --output DP-1 --off && \
-	nitrogen --random --set-zoom-fill
+	xrandr --output DP-1 --off --output HDMI-2 --mode 1920x1200 --rate 74.93 --pos 0x0 && \
+	nitrogen --random --set-zoom-fill --save
 else
 	echo "Extending desktop to second display." && \
-	xrandr --output DP-1 --mode 1920x1080 --pos 960x0 --output HDMI-2 --mode 1920x1200 --pos 0x1080 && \
-	echo -n --head=0,--head=1 | xargs -n 1 -d , nitrogen --random --set-zoom-fill
+	xrandr --output DP-1 --mode 1920x1080 --rate 60 --pos 960x0 --output HDMI-2 --mode 1920x1200 --rate 74.93 --pos 0x1080 && \
+	echo -n --head=0,--head=1 | xargs -n 1 -d , nitrogen --random --set-zoom-fill --save
 fi
