8d1e46ed5c5a4a987be92fe20ed2dad37716c482
AdolfGatonegro <github@gatoneg.ro>
2022-01-10 11:19:20 -0600

    Added ScratchPad with terminal

--- .config/qtile/groups.py
+++ .config/qtile/groups.py
@@ -1,4 +1,4 @@
-from libqtile.config import Group, Match, EzKey as Key
+from libqtile.config import Group, ScratchPad, DropDown, Match, EzKey as Key
 from libqtile.lazy import lazy
 
 from keys import keys
@@ -10,6 +10,13 @@ for i in groups:
         Key(f"M-{i.name}", lazy.group[i.name].toscreen(toggle=True), desc=f"Switch to group {i.name}"),
         Key(f"M-S-{i.name}", lazy.window.togroup(i.name, switch_group=True), desc=f"Send focused window to group {i.name} and switch"),
         Key(f"M-C-{i.name}", lazy.window.togroup(i.name), desc=f"Send focused window to group {i.name}")
+    ])
+
+groups.extend([ScratchPad("scratchpad", [
+    DropDown("term", "kitty", x=0.25, opacity = 0.8, width=0.5, height=0.5),])])
+
+keys.extend([
+    Key("<F12>", lazy.group['scratchpad'].dropdown_toggle('term')),
     ])
 
 groups[0].matches = [Match(wm_class = ["firefox"])]
