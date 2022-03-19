f6eafd31439c5c93db467e30d3f852220fc3d72a
AdolfGatonegro <github@gatoneg.ro>
2022-03-18 18:58:03 -0600

    add darktable match rule

--- .config/qtile/groups.py
+++ .config/qtile/groups.py
@@ -30,7 +30,7 @@ groups[1].matches = [Match(wm_class = ["Thunar","transmission-qt","transmission-
 groups[3].matches = [Match(wm_class = ["discord","mailspring"])]
 groups[4].matches = [Match(wm_class = ["DesktopEditors", "libreoffice", "libreoffice-writer"])]
 groups[5].matches = [Match(wm_class = ["Steam"])]
-groups[6].matches = [Match(wm_class = ["gimp-2.10","org.inkscape.Inkscape"])]
+groups[6].matches = [Match(wm_class = ["gimp-2.10","org.inkscape.Inkscape","darktable"])]
 # groups[7].matches = [Match(wm_class = [""])]
 groups[8].matches = [Match(wm_class = ["vmplayer","Vmplayer","virt-manager","VirtualBox Manager","VirtualBox Machine"])]
 
