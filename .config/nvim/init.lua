acc2e58380321c6906c248df8b39d25976bb04b8
adolfgatonegro <github@gatoneg.ro>
2022-10-10 10:48:06 -0500

    remove nvim config mess

--- .config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require "gatonegro.options"
-require "gatonegro.keymaps"
-require "gatonegro.plugins"
-require "gatonegro.colorscheme"
-require "gatonegro.cmp"
-require "gatonegro.lsp"
-require "gatonegro.treesitter"
