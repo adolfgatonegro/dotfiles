b5352f04821544860b5569303ec3ac2cf8b58456
adolfgatonegro <github@gatoneg.ro>
2024-09-08 23:58:27 -0600

    nvim: fix bufferline colours

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -3,8 +3,24 @@ return {
 
 	config = function ()
 		require("bufferline").setup{
+			highlights = {
+				buffer_selected = { bold = true, italic = false },
+				group_label = { bg = "NONE" },
+				separator = { fg = "NONE" },
+				fill = { bg = "NONE" },
+				separator_visible ={ fg = "NONE" },
+				separator_selected ={ fg = "NONE" },
+				offset_separator = {
+					big = "NONE",
+					italic = false,
+					bold = true,
+				},
+			},
 			options = {
-				themable = true,
+				themable = false,
+				indicator = { style = 'none'},
+				diagnostics = "nvim_lsp",
+				separator_style = "thick",
 			}
 		}
 	end,
