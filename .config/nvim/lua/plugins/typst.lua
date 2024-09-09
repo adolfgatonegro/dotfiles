4ec19f75319548c529249e0ea8d2cdca0df78249
adolfgatonegro <github@gatoneg.ro>
2024-09-08 18:18:54 -0600

    nvim: typst.vim conceal config still broken

--- .config/nvim/lua/plugins/typst.lua
+++ .config/nvim/lua/plugins/typst.lua
@@ -1,7 +1,8 @@
 return {
-	'kaarmu/typst.vim',
-	ft = 'typst',
+	"kaarmu/typst.vim",
+	ft = "typst",
+	-- This sets the option correctly, but conceal is not working. Why?
 	init = function()
-		vim.g["typst_conceal"] = 1
+		vim.g.typst_conceal = 1
 	end,
 }
