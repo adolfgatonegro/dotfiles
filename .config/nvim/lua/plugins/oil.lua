29ec1029b6a1ba5e7eeb768b84e6d896510d6a12
adolfgatonegro <github@gatoneg.ro>
2024-10-03 16:07:29 -0600

    nvim: add details in oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -6,6 +6,12 @@ return {
 	opts = {
 		delete_to_trash = true,
 		skip_confirm_for_simple_edits = true,
+		columns = {
+			"icon",
+			"permissions",
+			"size",
+			"mtime",
+		},
 		float = {
 			padding = 4,
 		},
