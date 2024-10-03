a022cf72628a5d8f0288833585349fe19fc9e613
adolfgatonegro <github@gatoneg.ro>
2024-10-02 18:20:33 -0600

    nvim: remove telescope-file-browser

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim", tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
+		-- "nvim-telescope/telescope-file-browser.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
 
@@ -44,16 +44,15 @@ return {
 					theme = "ivy",
 				},
 			},
-			extensions = {
-				file_browser = {
-					theme = "ivy",
-					hijack_netrw = false,
-					initial_mode = "normal",
-				},
-
-			},
+			-- extensions = {
+			-- 	file_browser = {
+			-- 		theme = "ivy",
+			-- 		hijack_netrw = false,
+			-- 		initial_mode = "normal",
+			-- 	},
+			-- },
 		})
-		require("telescope").load_extension("file_browser")
+		-- require("telescope").load_extension("file_browser")
 	end,
 	keys = {
 		{ "<leader>f", "<cmd>Telescope find_files<CR>", desc = "Telescope: Find files" },
