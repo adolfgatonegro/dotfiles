053e6bd7e4b8b66e34fcac7f9440bf0531934f1e
adolfgatonegro <github@gatoneg.ro>
2024-10-15 15:37:42 -0600

    emacs: bedrock early-init stuff

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,15 +1,12 @@
 ;; we're using elpaca, so disable package.el here
 (setq package-enable-at-startup nil)
 
-;; frame transparency parameters
-(set-frame-parameter (selected-frame) 'alpha '(100 . 100))
-(add-to-list 'default-frame-alist '(alpha . (100 . 100)))
-(set-frame-parameter nil 'alpha-background 90)
-(add-to-list 'default-frame-alist '(alpha-background . 90))
-
 ;; performance Hacks
-(setq gc-cons-threshold #x40000000)
+(setq byte-compile-warnings '(not obsolete))
+(setq gc-cons-threshold 10000000)
+(setq native-comp-async-report-warnings-errors 'silent)
 (setq read-process-output-max (* 1024 1024 4))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
 
 ;; minimal ui
 (menu-bar-mode -1) ;; disable menu bar
@@ -18,8 +15,25 @@
 (scroll-bar-mode -1) ;; disable scrollbar
 (pixel-scroll-precision-mode 1) ;; enable smooth scrolling
 
+(setq inhibit-startup-echo-area-message (user-login-name)) ;; silence startup message
+
 (setq inhibit-splash-screen t ;; disable splash screen
 use-file-dialog nil ;; don't use system file dialog
 tab-bar-new-button-show nil ;; don't show new tab button
 tab-bar-close-button-show nil ;; don't show tab close button
 tab-line-close-button-show nil) ;; don't show tab close button
+
+;; Default frame configuration: full screen, good-looking title bar on macOS
+(setq frame-resize-pixelwise t)
+(setq default-frame-alist '((fullscreen . maximized)
+                            ;; Setting the face in here prevents flashes of
+                            ;; color as the theme gets activated
+                            (background-color . "#0a0a15")
+                            (ns-appearance . dark)
+                            (ns-transparent-titlebar . t)))
+
+;; frame transparency parameters
+(set-frame-parameter (selected-frame) 'alpha '(100 . 100))
+(add-to-list 'default-frame-alist '(alpha . (100 . 100)))
+(set-frame-parameter nil 'alpha-background 90)
+(add-to-list 'default-frame-alist '(alpha-background . 90))
