c6f5ea3f609b21cb19fcbd65a1b83c0377b1e514
adolfgatonegro <github@gatoneg.ro>
2024-10-29 20:41:59 -0600

    emacs: theme add org-meta-line colour

--- .config/emacs/themes/doom-gatonegro-theme.el
+++ .config/emacs/themes/doom-gatonegro-theme.el
@@ -164,6 +164,7 @@ determine the exact padding."
    ((outline-7 &override) :foreground (doom-darken blue 0.4))
    ((outline-8 &override) :foreground (doom-darken green 0.4))
    ;;;; org <built-in>
+   ((org-meta-line &override) :foreground base6)
    ((org-block &override) :background base0)
    ((org-block-begin-line &override) :background base0)
    (org-hide              :foreground hidden)
